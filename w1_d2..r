6276ce1e3f45bb617880ce988ce129913f6b54a6
nghess <nhess@uoregon.edu>
2025-05-05 22:07:32 -0700

    607 bayes

---
+++ w1_d2..r
@@ -0,0 +1,9 @@
+# Load libraries, install if needed
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("cowplot")) install.packages("cowplot")
+
+library(tidyverse)
+library(cowplot)
+
+# Load data
+
